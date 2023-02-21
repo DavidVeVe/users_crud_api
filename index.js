191681e4639c164b57d44b56dc7426de81931664
David Velazquez <davidvelazquez@Davids-MacBook-Pro.local>
2023-02-20 20:23:47 -0600

    Add middlewares implementation and cors

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const express = require('express')
-const App = express()
+const express = require('express');
+const cors = require('cors')
+const routerApi = require('./routes');
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+const App = express();
+const port = 3000;
 
-const port = 3000
+App.use(cors())
+App.use(express.json());
+routerApi(App);
+
+App.use(logErrors);
+App.use(boomErrorHandler);
+App.use(errorHandler);
 
 App.listen(port, () => {
-    console.log(`Server up and running on port ${port}...`);
-})
+  console.log(`This is running on port ${port}`);
+});
